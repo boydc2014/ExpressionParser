af185c74aea4def98101c4d732a0f3ab0a769e14
Dong Lei <donglei@microsoft.com>
2021-07-05 19:58:41 +0800

    Simpify the way lexer units are added

--- ExpressionLexer.cs
+++ ExpressionLexer.cs
@@ -9,96 +9,32 @@ namespace Parser
         private int _index = 0;
         private string _text;
 
-        private Dictionary<TokenKind, Func<Token>> _tokenLexer = new Dictionary<TokenKind, Func<Token>>();
-        private Dictionary<TokenKind, string> _tokenFirsts = new Dictionary<TokenKind, string>();
+        // TokenKind => (SearchFunction, Firsts)
+        // SearchFuntion is a function that search for such a token
+        // Firsts is a string contains all possible chars to start this token
+        private Dictionary<TokenKind, (Func<string>, string)> _lexerUnits = new Dictionary<TokenKind, (Func<string>, string)>();
 
-        // reverse map, first char => possible lexer(s)
+        // Reverse map of _lexerUnits, first char => list of tokenizers
+        // This is data structure that drives the final lexer's behavior when facing each char
         private Dictionary<char, List<Func<Token>>> _lexerMap;
 
-
-        private Func<string> parseSingleChar(char c)
-        {
-            return () =>
-            {
-                if (_text[_index] == c)
-                {
-                    _index++;
-                    return c.ToString();
-                }
-                return null;
-            };
-        }
-
-        private Func<string> parseRegexPattern(string pattern) 
-        {
-            return () => 
-            {
-                var reg = new Regex(pattern);
-                var match = reg.Match(_text, _index);
-
-                if (match.Success)
-                {
-                    _index = _index + match.Length;
-                    return new Token(kind, _text.Substring(_index-match.Length, match.Length));
-                }
-                return null;
-            };
-        }
-
-        private string letters()
-        {
-            return "abcdefjhijklmnopqrstuvwxyzABCEDEFHIJKLMNOPQRSTUVWXYZ";
-        }
-
-        private string digits()
-        {
-            return "0123456789";
-        }
 
         public ExpressionLexer(string text)
         {
             _text = text;
-            _tokenLexer[TokenKind.PLUS] = parseSingleChar('+', TokenKind.PLUS);
-            _tokenFirsts[TokenKind.PLUS] = "+";
 
-            _tokenLexer[TokenKind.MINUS] = parseSingleChar('-', TokenKind.MINUS);
-            _tokenFirsts[TokenKind.MINUS] = "-";
-
-            _tokenLexer[TokenKind.MUL] = parseSingleChar('*', TokenKind.MUL);
-            _tokenFirsts[TokenKind.MUL] = "*";
+            _lexerUnits[TokenKind.PLUS] = (parseSingleChar('+'), "+");
+            _lexerUnits[TokenKind.MINUS] = (parseSingleChar('-'), "-");
+            _lexerUnits[TokenKind.MUL] = (parseSingleChar('*'), "*");
+            _lexerUnits[TokenKind.DIV] = (parseSingleChar('/'), "/");
             
-            _tokenLexer[TokenKind.ID] = parseRegexPattern(@"\G(_|@|#|\*|\$|@@)?[a-zA-Z][a-zA-Z0-9_]*", TokenKind.ID);
-            _tokenFirsts[TokenKind.ID] = "_$#@" + letters();
-
-            _tokenLexer[TokenKind.NUM] = parseRegexPattern(@"\G[0-9]+(\.[0-9]+)?", TokenKind.NUM);
-            _tokenFirsts[TokenKind.NUM] = digits();
+            _lexerUnits[TokenKind.ID] = (parseRegexPattern(@"\G(_|@|#|\*|\$|@@)?[a-zA-Z][a-zA-Z0-9_]*"), "_$#@" + letters());
+             _lexerUnits[TokenKind.NUM] = (parseRegexPattern(@"\G[0-9]+(\.[0-9]+)?"), digits());
 
             buildLexerMap();
         }
 
-        private void buildLexerMap()
-        {
-            _lexerMap = new Dictionary<char, List<Func<Token>>>();
-
-            foreach (var kv in _tokenLexer)
-            {
-                if (!_tokenFirsts.ContainsKey(kv.Key))
-                {
-                    throw new Exception($"Undefined firsts for: {kv.Key}");
-                }
-                var firsts = _tokenFirsts[kv.Key];
-
-                foreach (char c in firsts)
-                {
-                    if (!_lexerMap.ContainsKey(c))
-                    {
-                        _lexerMap[c] = new List<Func<Token>>();
-                    }
-                    _lexerMap[c].Add(kv.Value);
-                }
-            }
-        }
-
+        // Return the next token found, or EOF if hits the end
         public Token NextToken()
         {
             if (_index >= _text.Length)
@@ -139,5 +75,82 @@ namespace Parser
 
             _index++;
         }
+
+        // Build the reverse map _lexerMap based on _lexerUnits
+        private void buildLexerMap()
+        {
+            _lexerMap = new Dictionary<char, List<Func<Token>>>();
+
+            foreach (var kv in _lexerUnits)
+            {   
+                var firsts = kv.Value.Item2;
+
+                foreach (char c in firsts)
+                {
+                    if (!_lexerMap.ContainsKey(c))
+                    {
+                        _lexerMap[c] = new List<Func<Token>>();
+                    }
+                    _lexerMap[c].Add(createTokenier(kv.Value.Item1, kv.Key));
+                }
+            }
+        }
+
+        // Create a tokenier by binding a certain search function with the expected token kind to return
+        private Func<Token> createTokenier(Func<string> searchFunc, TokenKind kind)
+        {
+            return () => 
+            {
+                var result = searchFunc();
+                if (result != null) 
+                {
+                    return new Token(kind, result);
+                }
+                return null;
+            };
+        }
+
+        // Create a function that will look for a specific char in current position
+        private Func<string> parseSingleChar(char c)
+        {
+            return () =>
+            {
+                if (_text[_index] == c)
+                {
+                    _index++;
+                    return c.ToString();
+                }
+                return null;
+            };
+        }
+
+
+        // Create a function that search for a specific path in current position
+        private Func<string> parseRegexPattern(string pattern) 
+        {
+            return () => 
+            {
+                var reg = new Regex(pattern);
+                var match = reg.Match(_text, _index);
+
+                if (match.Success)
+                {
+                    _index = _index + match.Length;
+                    return _text.Substring(_index-match.Length, match.Length);
+                }
+                return null;
+            };
+        }
+
+        private string letters()
+        {
+            return "abcdefjhijklmnopqrstuvwxyzABCEDEFHIJKLMNOPQRSTUVWXYZ";
+        }
+
+        private string digits()
+        {
+            return "0123456789";
+        }
+
     }
 }
