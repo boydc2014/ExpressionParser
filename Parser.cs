324d4130122f9039b2acd35933f99d5fb747e87b
Dong Lei <donglei@microsoft.com>
2021-07-20 11:27:37 +0800

    Simpilfy combinators

--- Parser.cs
+++ Parser.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Text;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Parser
@@ -71,20 +73,20 @@ namespace Parser
                
         }
 
-        private Func<InputReader, ParserResult<string>> PChar(char c) 
+        private Func<InputReader, ParserResult<char>> Char(char c) 
         {
             return (input) => 
             {
                 var ch = input.Read();
                 if (ch == c)
                 {
-                    return ParserResult<string>.Success(c.ToString());
+                    return ParserResult<char>.Success(c);
                 }
-                return ParserResult<string>.Failure($"Unexpected input, expecting {c}, actual {ch}");
+                return ParserResult<char>.Failure($"Unexpected input, expecting {c}, actual {ch}");
             };
         }
 
-        private Func<InputReader, ParserResult<string>> PString(string str)
+        private Func<InputReader, ParserResult<string>> All(string str)
         {
             return (input) => 
             {
@@ -100,20 +102,23 @@ namespace Parser
             };
         }
 
-        private Func<InputReader, ParserResult<string>> AnyChar(string str) 
+        private Func<InputReader, ParserResult<char>> Any(string str) 
         {
             return (input) => 
             {
                 var ch = input.Read();
                 if (str.Contains(ch))
                 {
-                    return ParserResult<string>.Success(ch.ToString());
+                    return ParserResult<char>.Success(ch);
                 }
-                return ParserResult<string>.Failure($"Unexpected input, expecting any char in {str}, actual {ch}");
+                return ParserResult<char>.Failure($"Unexpected input, expecting any char in {str}, actual {ch}");
             };
         }
 
-
+        private Func<InputReader, ParserResult<string>> Str<T>(Func<InputReader, ParserResult<T>> parser)
+        {
+            return Select<T, string>(parser, v => v.ToString());
+        }
 
         private Func<InputReader, ParserResult<T>> Try<T>(Func<InputReader, ParserResult<T>> parser)
         {
@@ -145,7 +150,7 @@ namespace Parser
             };
         }
 
-        private Func<InputReader, ParserResult<T>> Opt<T>(Func<InputReader, ParserResult<T>> parser, T defaultValue)
+        private Func<InputReader, ParserResult<T>> Opt<T>(Func<InputReader, ParserResult<T>> parser, T defaultValue = default(T))
         {
             return (input) => 
             {
@@ -191,6 +196,61 @@ namespace Parser
             };
         }
 
+        private Func<InputReader, ParserResult<string>> Many(Func<InputReader, ParserResult<char>> parser)
+        {
+            return (input) => 
+            {
+                var builder = new StringBuilder();
+                while(true) 
+                {
+                    var part = Try<char>(parser)(input);
+                    if (part.IsSuccess)
+                    {
+                        builder.Append(part.Value);
+                    }
+                    else 
+                    {
+                        break;
+                    }
+                }
+                return ParserResult<string>.Success(builder.ToString());
+            };
+        }
+
+        private Func<InputReader, ParserResult<IEnumerable<T>>> Many1<T>(Func<InputReader, ParserResult<T>> parser)
+        {
+            return (input) => 
+            {
+                var result = new List<T>(){};
+                var item0 = parser(input);
+                if (item0.IsSuccess)
+                {
+                    result.Add(item0.Value);
+                }
+                else
+                {
+                    return ParserResult<IEnumerable<T>>.Failure(item0.ErrorMessage);
+                }
+                var rest = Many<T>(parser)(input);
+                result.AddRange(rest.Value);
+                return ParserResult<IEnumerable<T>>.Success(result);
+            };
+        }
+
+        private Func<InputReader, ParserResult<string>> Many1(Func<InputReader, ParserResult<char>> parser)
+        {
+            return (input) => 
+            {
+                var item0 = parser(input);
+                if (!item0.IsSuccess)
+                {
+                    return ParserResult<string>.Failure(item0.ErrorMessage);
+                }
+                var rest = Many(parser)(input);
+                return ParserResult<string>.Success(item0.Value.ToString() + rest.Value);
+            };
+        }
+
         private Func<InputReader, ParserResult<IEnumerable<T>>> Seq<T>(params Func<InputReader, ParserResult<T>>[] parsers)
         {
             return (input) => 
@@ -212,26 +272,52 @@ namespace Parser
             };
         }
 
+        private Func<InputReader, ParserResult<T>> Aggregate<T>(Func<InputReader, ParserResult<T>>[] parsers, Func<T, T, T> func)
+        {
+            return (input) =>
+            {
+                var result = Seq(parsers)(input);
+                if (result.IsSuccess)
+                {
+                    var finalValue = result.Value.Aggregate(func);
+                    return ParserResult<T>.Success(finalValue);
+                }
+                else
+                {
+                    return ParserResult<T>.Failure(result.ErrorMessage);
+                }
+            };
+        }
+
+        private Func<InputReader, ParserResult<string>> Concat(params Func<InputReader, ParserResult<string>>[] parsers)
+        {
+            return Aggregate(parsers, (x, y) => x + y);
+        }
+
+        private string ConcatStrs(IEnumerable<string> strs)
+        {
+            return string.Join("", strs);
+        }
 
         public ParserResult<SyntaxNode> Parse(string input)
         {
             var inputReader = new InputReader(input);
-            var parseID = Select(Seq(Opt(Or(PString("@@"), AnyChar("_#$@%")), string.Empty),
-                                     AnyChar(letters()),
-                                     Select(Many(AnyChar(letters() + digits() + "_")), strs => string.Join("", strs))),
-                                 strs => string.Join("", strs));
+            var ID = Concat(Opt(Or(All("@@"), Str(Any("_#$@%")))), Str(Any(Letter())), Many(Any(Letter() + Digit() + "_")));
             
-            parseExpression = Select(parseID, id => new SyntaxNode(new Token(TokenKind.ID, id)));
+            var manyDigits = Many1(Any(Digit()));
+            var NUM = Concat(manyDigits, Opt(Concat(Str(Char('.')), manyDigits)));
+            
+            parseExpression = Select(ID, id => new SyntaxNode(new Token(TokenKind.ID, id)));
 
             return parseExpression(inputReader);
         }
 
-        private string letters()
+        private string Letter()
         {
             return "abcdefghijklmnopqrstuvwxyzABCEDEFGHIJKLMNOPQRSTUVWXYZ";
         }
 
-        private string digits()
+        private string Digit()
         {
             return "0123456789";
         }
